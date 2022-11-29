171591f47abdf4a9ab1b4721fa71f3be8f4d5e4f
Roman Jelinek <jelinek82@gmail.com>
2022-11-29 15:27:15 +0100

    Nav, contacts and config changes

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  i18n: {
+    locales: ['cs'],
+    defaultLocale: 'cs',
+  },
 };
 
 module.exports = nextConfig;
