959f5aa4f9a10632c3b127f77be8740c106c7ccc
Roman Jelinek <jelinek82@gmail.com>
2022-11-29 16:22:09 +0100

    Head tags in subpages

--- pages/kontakty.js
+++ pages/kontakty.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
 import {
     BsChevronDoubleLeft,
   } from "react-icons/bs";
@@ -10,25 +11,36 @@ function kontakty() {
 
 
   return (
-    <div className='row'>
-        <div className='col-12 mb-4'>
-            <a onClick={() => router.back()} className={styles.backLink}>
-                <BsChevronDoubleLeft /> zpět
-            </a>
-        </div>
-        <div className='col-12' id={styles.contactBox}>
-            <h1>
-                Kontakty
-            </h1>
-            
-            <p>Vinařství Iris s.r.o.</p>
-            <p>Podhradní 180</p>
-            <p>692 01 Pavlov</p>
-            <p>e-mail: <a href="mailto:obchod@vinarstviiris.cz">obchod@vinarstviiris.cz</a></p>
-            <p>telefon: <a href="tel:+420602528545">+420 602 528 545</a></p>
+    <div>
+        <Head>
+        <title>Kontakty - Vinařství Iris</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name='description' content='Kontakty Vinařství Iris Pavlov - rodinné vinařství z Pavlova' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.0834122236547!2d16.665920815903487!3d48.87568640744245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4712cdcdfdeefbe9%3A0x1a3830fe50f8b6a5!2sPodhradn%C3%AD%20180%2C%20692%2001%20Pavlov!5e0!3m2!1scs!2scz!4v1614621074531!5m2!1scs!2scz" width="600" height="450" loading="lazy"></iframe>
+        <div className='row'>
+            <div className='col-12 mb-4'>
+                <a onClick={() => router.back()} className={styles.backLink}>
+                    <BsChevronDoubleLeft /> zpět
+                </a>
+            </div>
+            <div className='col-12' id={styles.contactBox}>
+                <h1>
+                    Kontakty
+                </h1>
+
+                <p>Vinařství Iris s.r.o.</p>
+                <p>Podhradní 180</p>
+                <p>692 01 Pavlov</p>
+                <p>e-mail: <a href="mailto:obchod@vinarstviiris.cz">obchod@vinarstviiris.cz</a></p>
+                <p>telefon: <a href="tel:+420602528545">+420 602 528 545</a></p>
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.0834122236547!2d16.665920815903487!3d48.87568640744245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4712cdcdfdeefbe9%3A0x1a3830fe50f8b6a5!2sPodhradn%C3%AD%20180%2C%20692%2001%20Pavlov!5e0!3m2!1scs!2scz!4v1614621074531!5m2!1scs!2scz" width="600" height="450" loading="lazy"></iframe>
+            </div>
         </div>
+    
     </div>
   )
 }
--- pages/produkt/[slug].js
+++ pages/produkt/[slug].js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { GET_PRODUCT_QUERY } from "../../lib/query";
 import { useQuery } from "urql";
 import { useRouter } from "next/router";
@@ -49,64 +50,74 @@ export default function ProductDetails() {
   } = data.products.data[0].attributes;
 
   return (
-    <div className='row'>
-      <div className='col-12 mb-4'>
-        <a onClick={() => router.back()} className={styles.backLink}>
-          <BsChevronDoubleLeft /> zpět
-        </a>
-      </div>
-      <div className='col-12 col-lg-6'>
-        <img src={image.data.attributes.formats.medium.url} alt={name} />
-      </div>
-      <div className='col-col-12 col-lg-6 mb-4'>
-        <div className={styles.detailText}>
-          <h1>
-            {name} {year} <br />{attribute}
-          </h1>
-          <p>{sweetness}</p>
-          <div className={styles.quantityBox}>
-            <div onClick={decreaseQty}>
-              <BsDashSquare />
+    <div>
+      <Head>
+        <title>{name} {year} {attribute} - Vinařství Iris</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name='description' content={`${name} ${year} ${attribute} - Vinařství Iris Pavlov - rodinné vinařství z Pavlova`} />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    
+      <div className='row'>
+        <div className='col-12 mb-4'>
+          <a onClick={() => router.back()} className={styles.backLink}>
+            <BsChevronDoubleLeft /> zpět
+          </a>
+        </div>
+        <div className='col-12 col-lg-6'>
+          <img src={image.data.attributes.formats.medium.url} alt={name} />
+        </div>
+        <div className='col-col-12 col-lg-6 mb-4'>
+          <div className={styles.detailText}>
+            <h1>
+              {name} {year} <br />{attribute}
+            </h1>
+            <p>{sweetness}</p>
+            <div className={styles.quantityBox}>
+              <div onClick={decreaseQty}>
+                <BsDashSquare />
+              </div>
+              <p>{qty}</p>
+              <div onClick={increaseQty}>
+                <BsPlusSquare />
+              </div>
             </div>
-            <p>{qty}</p>
-            <div onClick={increaseQty}>
-              <BsPlusSquare />
-            </div>
+            <button
+              onClick={() => {
+                onAdd(data.products.data[0].attributes, qty);
+              }}
+              className='btn btn-primary'
+            >
+              Přidat do košíku
+            </button>
+            {description != null && <p>{description}</p>}
+            {color != null && (
+              <p>
+                <strong>Barva:</strong> {color}
+              </p>
+            )}
+            {alcohol != null && (
+              <p>
+                <strong>Alkohol:</strong> {alcohol}
+              </p>
+            )}
+            {sugar != null && (
+              <p>
+                <strong>Zbytkový cukr:</strong> {sugar}
+              </p>
+            )}
+            {acid != null && (
+              <p>
+                <strong>Kyselinky:</strong> {acid}
+              </p>
+            )}
+            {locality != null && (
+              <p>
+                <strong>Obec:</strong> {locality}
+              </p>
+            )}
           </div>
-          <button
-            onClick={() => {
-              onAdd(data.products.data[0].attributes, qty);
-            }}
-            className='btn btn-primary'
-          >
-            Přidat do košíku
-          </button>
-          {description != null && <p>{description}</p>}
-          {color != null && (
-            <p>
-              <strong>Barva:</strong> {color}
-            </p>
-          )}
-          {alcohol != null && (
-            <p>
-              <strong>Alkohol:</strong> {alcohol}
-            </p>
-          )}
-          {sugar != null && (
-            <p>
-              <strong>Zbytkový cukr:</strong> {sugar}
-            </p>
-          )}
-          {acid != null && (
-            <p>
-              <strong>Kyselinky:</strong> {acid}
-            </p>
-          )}
-          {locality != null && (
-            <p>
-              <strong>Obec:</strong> {locality}
-            </p>
-          )}
         </div>
       </div>
     </div>
