15c54a72b3270e449534cb38b37cce27797702ec
Roman Jelinek <jelinek82@gmail.com>
2022-12-12 11:42:04 +0100

    OG tags added to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.scss";
+import Head from "next/head";
 import { Provider, createClient } from "urql";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
@@ -10,6 +11,19 @@ const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API });
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
+      <Head>
+        <title>Vinařství Iris</title>
+        <meta name="title" content="Vinařství Iris" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name='description' content='Vinařství Iris Pavlov - rodinné vinařství z Pavlova' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta property="og:title" content="Vinařství Iris" />
+        <meta property="og:type" content="website"/>
+        <meta property="og:description" content="Vinařství Iris Pavlov - rodinné vinařství z Pavlova." />
+        <meta property="og:image" content="/bottlePal.png" />
+        <meta property="og:url" content="https://irispavlov.cz" />
+      </Head>
       <Provider value={client}>
         <Nav />
         <div className='container'>
