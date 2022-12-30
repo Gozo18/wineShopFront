6fa289023af69b1e8c77e93afa05447011f31c72
Roman Jelinek <jelinek82@gmail.com>
2022-12-30 14:03:56 +0100

    404 added + font added

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import { GiWineBottle } from "react-icons/gi";
+import styles from "../styles/404.module.scss";
+
+export default function FourOhFour() {
+  return (
+    <div className={styles.errorBox}>
+        <div className={styles.errorIcon}><GiWineBottle /></div>
+        <h1>404 - Stránka nenalezena</h1>
+        <Link href="/">
+            zpět na hlavní stránku
+        </Link>
+    </div>
+  )
+}
