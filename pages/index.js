04cb9335045ccf52cea3a1681ce0413bb2d80334
Roman Jelinek <jelinek82@gmail.com>
2023-04-06 15:22:55 +0200

    Loading homepage - Index changed

--- pages/index.js
+++ pages/index.js
@@ -50,89 +50,90 @@ export default function Home() {
 
   if (!data) {
     router.reload(window.location.pathname)
-  }
-  const products = data.products.data
+  } else {
+    const products = data.products.data
 
-  const closeModal = () => {
-    setShowWarning(false)
-  }
+    const closeModal = () => {
+      setShowWarning(false)
+    }
 
-  return (
-    <div className="homepage">
-      <Head>
-        <title>Vinařství Iris</title>
-        <meta name="title" content="Vinařství Iris" />
-        <meta
-          name="description"
-          content="Vinařství Iris Pavlov - rodinné vinařství z Pavlova"
-        />
+    return (
+      <div className="homepage">
+        <Head>
+          <title>Vinařství Iris</title>
+          <meta name="title" content="Vinařství Iris" />
+          <meta
+            name="description"
+            content="Vinařství Iris Pavlov - rodinné vinařství z Pavlova"
+          />
 
-        <meta property="og:title" content="Vinařství Iris" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:description"
-          content="Vinařství Iris Pavlov - rodinné vinařství z Pavlova."
-        />
-        <meta
-          property="og:image"
-          content="https://wine-shop-front.vercel.app/bottlePal.png"
-        />
-        <meta property="og:url" content="https://irispavlov.cz" />
-      </Head>
+          <meta property="og:title" content="Vinařství Iris" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:description"
+            content="Vinařství Iris Pavlov - rodinné vinařství z Pavlova."
+          />
+          <meta
+            property="og:image"
+            content="https://wine-shop-front.vercel.app/bottlePal.png"
+          />
+          <meta property="og:url" content="https://irispavlov.cz" />
+        </Head>
 
-      <main>
-        <Header />
-        <h2 className={styles.wineHeader}>Naše vína</h2>
-        <div className={styles.filterBox}>
-          <Link href="/bila-vina">
-            <a className={styles.whiteWine}>bílá vína</a>
-          </Link>
-          <Link href="/cervena-vina">
-            <a className={styles.redWine}>červená vína</a>
-          </Link>
-          <Link href="/ruzova-vina">
-            <a className={styles.roseWine}>růžová vína</a>
-          </Link>
-        </div>
-        <div className="row">
-          {products.map((product) => (
-            <Product key={product.attributes.slug} product={product} />
-          ))}
-        </div>
-      </main>
-      {showWarning && (
-        <div className="homepageOverlay">
-          <div className="modal">
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5 className="modal-title">Vinařství Iris</h5>
-                  <button
-                    type="button"
-                    className="btn-close"
-                    onClick={closeModal}
-                  ></button>
-                </div>
-                <div className="modal-body">
-                  <p>
-                    Varování - pokud vám není 18 a více let, prosím, opusťte
-                    tyto stránky.
-                  </p>
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    onClick={closeModal}
-                  >
-                    Zavřít
-                  </button>
+        <main>
+          <Header />
+          <h2 className={styles.wineHeader}>Naše vína</h2>
+          <div className={styles.filterBox}>
+            <Link href="/bila-vina">
+              <a className={styles.whiteWine}>bílá vína</a>
+            </Link>
+            <Link href="/cervena-vina">
+              <a className={styles.redWine}>červená vína</a>
+            </Link>
+            <Link href="/ruzova-vina">
+              <a className={styles.roseWine}>růžová vína</a>
+            </Link>
+          </div>
+          <div className="row">
+            {products.map((product) => (
+              <Product key={product.attributes.slug} product={product} />
+            ))}
+          </div>
+        </main>
+        {showWarning && (
+          <div className="homepageOverlay">
+            <div className="modal">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title">Vinařství Iris</h5>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      onClick={closeModal}
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    <p>
+                      Varování - pokud vám není 18 a více let, prosím, opusťte
+                      tyto stránky.
+                    </p>
+                  </div>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={closeModal}
+                    >
+                      Zavřít
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
-  )
+        )}
+      </div>
+    )
+  }
 }
