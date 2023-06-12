1d799d459d57d295d3c6dadcac2e6eab6f2c9d26
Roman Jelinek <jelinek82@gmail.com>
2023-06-12 10:55:01 +0200

    GA added

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,44 @@
-import "../styles/globals.scss";
-import Head from "next/head";
-import { Provider, createClient } from "urql";
-import Nav from "../components/Nav";
-import Footer from "../components/Footer";
-import { StateContext } from "../lib/context";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.scss"
+import Head from "next/head"
+import { Provider, createClient } from "urql"
+import Nav from "../components/Nav"
+import Footer from "../components/Footer"
+import { StateContext } from "../lib/context"
+import "bootstrap/dist/css/bootstrap.min.css"
 
-const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API });
+const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API })
 
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Q1XH9M80QQ"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Q1XH9M80QQ');
+        `,
+          }}
+        />
       </Head>
       <Provider value={client}>
         <Nav />
-        <div className='container'>
+        <div className="container">
           <Component {...pageProps} />
           <Footer />
         </div>
       </Provider>
     </StateContext>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
