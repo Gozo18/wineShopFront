b1a18c91a3587d98facb906172e07561064c4810
Roman Jelinek <jelinek82@gmail.com>
2023-07-11 10:38:01 +0200

    Nav heading change

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,20 @@
-import Link from "next/link";
-import Image from 'next/image';
-import { HiOutlineShoppingCart, HiOutlineIdentification, HiOutlineHome, HiOutlineRectangleStack } from "react-icons/hi2";
-import Cart from "./Cart";
-import { useStateContext } from "../lib/context";
-import { withRouter } from "next/router";
-const { AnimatePresence, motion } = require("framer-motion");
-import "bootstrap/dist/css/bootstrap.min.css";
-import styles from "../styles/Nav.module.scss";
+import Link from "next/link"
+import Image from "next/image"
+import {
+  HiOutlineShoppingCart,
+  HiOutlineIdentification,
+  HiOutlineHome,
+  HiOutlineRectangleStack,
+} from "react-icons/hi2"
+import Cart from "./Cart"
+import { useStateContext } from "../lib/context"
+import { withRouter } from "next/router"
+const { AnimatePresence, motion } = require("framer-motion")
+import "bootstrap/dist/css/bootstrap.min.css"
+import styles from "../styles/Nav.module.scss"
 
 function Nav({ router }) {
-  const { showCart, setShowCart, totalQuantities } = useStateContext();
+  const { showCart, setShowCart, totalQuantities } = useStateContext()
 
   return (
     <div className={styles.navSpace}>
@@ -17,56 +22,60 @@ function Nav({ router }) {
         <div className="container">
           <div className={styles.navBox}>
             <div className={styles.nav}>
-              <Link href='/'>
+              <Link href="/">
                 <a className={styles.logoImage}>
-                  <Image
-                    src='/logo_only.png' alt='logo'
-                    layout="fill"
-                  />
+                  <Image src="/logo_only.png" alt="logo" layout="fill" />
                 </a>
               </Link>
               <div className={styles.navMenu}>
-              {(router.pathname != "/kosik") & (router.pathname != "/objednavka") ? (
-                <>
-                  <div onClick={() => setShowCart(true)} className={styles.navCart}>
-                    {totalQuantities > 0 && (
-                      <motion.span animate={{ scale: 1 }} initial={{ scale: 0 }}>
-                        {totalQuantities}
-                      </motion.span>
-                    )}
-                    <HiOutlineShoppingCart />
-                    <h3>Košík</h3>
+                {(router.pathname != "/kosik") &
+                (router.pathname != "/objednavka") ? (
+                  <>
+                    <div
+                      onClick={() => setShowCart(true)}
+                      className={styles.navCart}
+                    >
+                      {totalQuantities > 0 && (
+                        <motion.span
+                          animate={{ scale: 1 }}
+                          initial={{ scale: 0 }}
+                        >
+                          {totalQuantities}
+                        </motion.span>
+                      )}
+                      <HiOutlineShoppingCart />
+                      <div className={styles.navheading}>Košík</div>
+                    </div>
+                    <AnimatePresence>{showCart && <Cart />}</AnimatePresence>
+                  </>
+                ) : (
+                  <></>
+                )}
+                <Link href="/velkoodber">
+                  <div className={styles.navContacts}>
+                    <HiOutlineRectangleStack />
+                    <div className={styles.navheading}>Velkoodběr</div>
                   </div>
-                  <AnimatePresence>{showCart && <Cart />}</AnimatePresence>
-                </>
-              ) : (
-                <></>
-              )}
-              <Link href='/velkoodber'>
-                <div className={styles.navContacts}>
-                  <HiOutlineRectangleStack />
-                  <h3>Velkoodběr</h3>
-                </div>
-              </Link>
-              <Link href='/kontakty'>
-                <div className={styles.navContacts}>
-                  <HiOutlineIdentification />
-                  <h3>Kontakty</h3>
-                </div>
-              </Link>
-              <Link href='https://vilapavlov.cz'>
-                <div className={styles.navContacts}>
-                  <HiOutlineHome />
-                  <h3>Ubytování</h3>
-                </div>
-              </Link>
+                </Link>
+                <Link href="/kontakty">
+                  <div className={styles.navContacts}>
+                    <HiOutlineIdentification />
+                    <div className={styles.navheading}>Kontakty</div>
+                  </div>
+                </Link>
+                <Link href="https://vilapavlov.cz">
+                  <div className={styles.navContacts}>
+                    <HiOutlineHome />
+                    <div className={styles.navheading}>Ubytování</div>
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default withRouter(Nav);
+export default withRouter(Nav)
