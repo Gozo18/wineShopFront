5d6a5fc9499f69fdfcbdeefdf354ca6c97622e94
Roman Jelinek <jelinek82@gmail.com>
2023-07-20 08:43:26 +0200

    Robots.txt modified and sitemap.xml added

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,65 @@
+const EXTERNAL_DATA_URL =
+  "https://strapi-production-16e2.up.railway.app/api/products"
+const PAGE_URL = "https://www.vinarstviiris.cz/produkt/"
+const URL = "https://www.vinarstviiris.cz/"
+
+function generateSiteMap(products) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>${URL}</loc>
+     </url>
+     <url>
+       <loc>${URL}/bila-vina</loc>
+     </url>
+     <url>
+       <loc>${URL}/cervena-vina</loc>
+     </url>
+     <url>
+       <loc>${URL}/ruzova-vina</loc>
+     </url>
+     <url>
+       <loc>${URL}/velkoodber</loc>
+     </url>
+      <url>
+       <loc>${URL}/kontakty</loc>
+     </url>
+     ${products
+       .map((product) => {
+         return `
+       <url>
+           <loc>${`${PAGE_URL}/${product.attributes.slug}`}</loc>
+       </url>
+     `
+       })
+       .join("")}
+   </urlset>
+ `
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(EXTERNAL_DATA_URL)
+  const result = await request.json()
+
+  if (result) {
+    const products = result.data
+
+    // We generate the XML sitemap with the posts data
+    const sitemap = generateSiteMap(products)
+
+    res.setHeader("Content-Type", "text/xml")
+    // we send the XML to the browser
+    res.write(sitemap)
+    res.end()
+  }
+  return {
+    props: {},
+  }
+}
+
+export default SiteMap
